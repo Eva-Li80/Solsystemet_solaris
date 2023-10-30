e09205a0b24e8c8808fac8e2c18a6c098e7cc15c
Eva-Li80 <evaliboden@gmail.com>
2023-10-30 14:02:39 +0100

    add eventlistener to every planet

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const url = "https://majazocom.github.io/Data/solaris.json";
 
+//säkerthetsställer att document laddas innan javascript
 document.addEventListener("DOMContentLoaded", function () {
 
+  const sun = document.querySelector("#sun");
   const mercurius = document.querySelector("#mercurius");
+  const venus = document.querySelector("#venus");
+  const earth = document.querySelector("#earth");
+  const mars = document.querySelector("#mars");
+  const jupiter = document.querySelector("#jupiter");
+  const saturnus = document.querySelector("#saturnus");
+  const uranus = document.querySelector("#mercurius");
+  const neptunus = document.querySelector("#neptunus");
 
+
+  const solen = document.querySelector("#sun")
   const about = document.querySelector("#about-planet");
   const name = document.querySelector("#planet-name");
   let currentPlanet;
@@ -34,6 +45,8 @@ document.addEventListener("DOMContentLoaded", function () {
       if (planet) {
         createContent(planet);
         currentPlanet = infoPlanet;
+        solen.style.backgroundColor = "red"
+        
       } else {
         console.log("Something went wrong!");
       }
@@ -42,7 +55,31 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  sun.addEventListener("click", async () => {
+    await displayInfoFromPlanet("solen");
+  });
   mercurius.addEventListener("click", async () => {
     await displayInfoFromPlanet("merkurius");
+  });
+  venus.addEventListener("click", async () => {
+    await displayInfoFromPlanet("merkurius");
+  });
+  earth.addEventListener("click", async () => {
+    await displayInfoFromPlanet("jorden");
+  });
+  mars.addEventListener("click", async () => {
+    await displayInfoFromPlanet("mars");
+  });
+  jupiter.addEventListener("click", async () => {
+    await displayInfoFromPlanet("jupiter");
+  });
+  saturnus.addEventListener("click", async () => {
+    await displayInfoFromPlanet("saturnus");
+  });
+  uranus.addEventListener("click", async () => {
+    await displayInfoFromPlanet("uranus");
+  });
+  neptunus.addEventListener("click", async () => {
+    await displayInfoFromPlanet("neptunus");
   });
 });
